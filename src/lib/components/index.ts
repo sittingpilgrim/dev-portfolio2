15ba6b09b94a4a681e70aa7aa42269b1d7cee72c
Tom Schmidt <tom.solstice20@gmail.com>
2025-01-09 17:12:04 -0800

    thru AboutMe section

---
+++ src/lib/components/index.ts
@@ -0,0 +1,6 @@
+
+export { default as Button } from './Button.svelte';
+export { default as SectionHeadline } from './SectionHeadline.svelte';
+
+export { default as HeroSection } from './sections/HeroSection.svelte';
+export { default as AboutMeSection } from './sections/AboutMeSection.svelte';
---
+++ src/lib/components/sections/HeroSection.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { Button } from "$components";
+
+    function onclick() {
+        goto("/#contact-form");
+    }
+</script>
+
+<section class="hero-section default-margin">
+    <h3>Hello I'm Tom</h3>
+    <div class="underscore mb-m"></div>
+    <h2 class="mb-s">I'm a web application developer based in the US building web applications with SvelteKit.</h2>
+    <Button {onclick}>Let's Talk</Button>
+</section>
+
+
+<style>
+    .hero-section {
+        padding-top: 60px;
+    }
+
+</style>
+
